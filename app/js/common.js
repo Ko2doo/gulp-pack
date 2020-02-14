655373f9d750a9915a6a589fddb189a1619c0c60
NИ <Ko2dooDev@gmail.com>
2020-02-14 15:01:24 +0500

    Update:14.02.2020 => Обновление сборщика, добавлена сетка smart-grid, и несколько часто используемых пакетов

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,4 @@
-$(document).ready(function(){
-  //Elastic Search
-  $('#search').click(function(elasticSearch){
-    elasticSearch.preventDefault();
-      $('#search').removeClass('close').addClass('active');
-      $('.btn-close').css({"display" : "flex"});
-  });
-  
-  $('.btn-close').click(function(elasticSearch){
-    $('.btn-close').css({"display" : "none"});
-    $('#search.active').removeClass('active').addClass('close');
-  });
-  
-  
-  
-  
-});//end
+$(function(){
 
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-var gulp        = require('gulp'),
-  sass          = require('gulp-sass'),
-  browserSync   = require('browser-sync'),
-  gcmq          = require('gulp-group-css-media-queries'),
-  concat        = require('gulp-concat'),
-  uglify        = require('gulp-uglifyjs'),
-  cssnano       = require('gulp-cssnano'),
-  rename        = require('gulp-rename'),
-  del           = require('del'),
-  cache         = require('gulp-cache'),
-  autoprefixer  = require('gulp-autoprefixer');
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    smartgrid     = require('smart-grid'),
+    browserSync   = require('browser-sync'),
+    addsrc        = require('gulp-add-src'),
+    gcmq          = require('gulp-group-css-media-queries'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglifyjs'),
+    cssnano       = require('gulp-cssnano'),
+    rename        = require('gulp-rename'),
+    del           = require('del'),
+    cache         = require('gulp-cache'),
+    autoprefixer  = require('gulp-autoprefixer');
 
 
 // Таск для Sass
@@ -19,12 +21,13 @@ gulp.task('sass', async function() {
         errorLogToConsole: true
       })).on('error', sass.logError)
     .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gcmq()) //группировка медиазапросов
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
 //таск для синхонизации с браузером
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', async function() {
   browserSync({
     server: {
       baseDir: 'app'
@@ -33,15 +36,40 @@ gulp.task('browser-sync', function() {
   });
 });
 
-//Таск для всех сприптов
-gulp.task('scripts', function() {
-  return gulp.src([
-    'app/libs/jquery/dist/jquery.min.js',
-    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
-    ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+// настройки сетки smart-grid
+gulp.task('smart-grid', (cb) => {
+  smartgrid('app/scss/stylesheets/', {
+    outputStyle: 'scss',
+    filename: '_smart-grid',
+    columns: 12, // number of grid columns
+    offset: '1.875rem', // gutter width - 30px
+    mobileFirst: false,
+    mixinNames: {
+        container: 'container'
+    },
+    container: {
+      maxWidth: '1170px',
+      fields: '0.9375rem' // side fields - 15px
+    },
+    breakPoints: {
+      xs: {
+          width: '20rem' // 320px
+      },
+      sm: {
+          width: '36rem' // 576px
+      },
+      md: {
+          width: '48rem' // 768px
+      },
+      lg: {
+          width: '62rem' // 992px
+      },
+      xl: {
+          width: '75rem' // 1200px
+      }
+    }
+  });
+  cb();
 });
 
 gulp.task('code', function() {
@@ -49,62 +77,70 @@ gulp.task('code', function() {
   .pipe(browserSync.reload({ stream: true }))
 });
 
-// минификация css
-gulp.task('css-libs', function() {
+// Объединяем все js либы в один файл
+gulp.task('scripts', async function() {
+  return gulp.src(['node_modules/jquery/dist/jquery.js'])
+    .pipe(addsrc.append('node_modules/magnific-popup/dist/jquery.magnific-popup.js'))
+    .pipe(concat('libs.js'))
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// объединям все css библиотеки в одну
+gulp.task('css-lib', function() {
   return gulp.src([
-    'app/css/libs.css',
-    'app/css/main.css'
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
-    .pipe(cssnano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(concat('libs.css'))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-
-// Группируем медиа-запросы
-gulp.task('media-queries', function (){
-  return gulp.src('app/css/**/*.css')
-    .pipe(gcmq())
-    .pipe(gulp.dest('dist/css'));
-});
 
 gulp.task('clean', async function() {
   return del.sync('dist');
 });
 
-gulp.task('img', function() {
-  return gulp.src('app/img/**/*')
-    .pipe(gulp.dest('dist/img'));
+gulp.task('clear-cache', function (callback) {
+  return cache.clearAll();
 });
 
-gulp.task('prebuild', async function() {
-
-  var buildCss = gulp.src([
-    'app/css/**/*.css'
-    ])
-  .pipe(gcmq())
-  .pipe(gulp.dest('dist/css'))
+gulp.task('prebuild', async function(){
+  var buildCSS = gulp.src(['app/css/**/*.css'])
+      .pipe(cssnano()) // минификация
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist/css'))
 
   var buildFonts = gulp.src('app/fonts/**/*')
-  .pipe(gulp.dest('dist/fonts'))
+      .pipe(gulp.dest('dist/fonts'))
 
-  var buildJs = gulp.src('app/js/**/*')
-  .pipe(gulp.dest('dist/js'))
+  var buildIMG = gulp.src('app/img/**/*')
+      .pipe(gulp.dest('dist/img'))
 
-  var buildHtml = gulp.src('app/**/*.html')
-  .pipe(gulp.dest('dist'));
+  var buildJSLibs = gulp.src('app/js/libs.js')
+      .pipe(uglify())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist/js'))
 
+  var buildJSCommon = gulp.src('app/js/common.js')
+      .pipe(uglify())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist/js'))
+
+  var buildHTML = gulp.src('app/**/*.html')
+      .pipe(gulp.dest('dist'))
 });
 
-gulp.task('clear', function (callback) {
-  return cache.clearAll();
-})
-
+// Следим за файлами
 gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
   gulp.watch('app/**/*.html', gulp.parallel('code'));
-  gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('scripts'));
+  gulp.watch(['app/js/common.js'], gulp.parallel('scripts'));
 });
 
-gulp.task('default', gulp.parallel('sass', 'scripts', 'browser-sync', 'watch'));
-gulp.task('build', gulp.parallel('clear', 'clean', 'css-libs', 'prebuild', 'img'));
+gulp.task('default',
+     gulp.parallel('clear-cache', 'smart-grid', 'sass', 'css-lib', 'scripts', 'browser-sync', 'watch'));
+
+gulp.task('build',
+     gulp.series('clean', 'clear-cache', 'prebuild'));
